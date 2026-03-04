41c0218a2ec3e8e1cbc20fb0293ed4a41e101217
nielassis <nielvitorba@gmail.com>
2026-03-04 15:35:47 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", () => {
+  return { message: "Hello World!" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8080 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
